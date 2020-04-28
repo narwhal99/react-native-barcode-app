56f367ede45cc14ee47290a34ed25ade9826e244
Molnár Andrej <molnarandre@outlook.com>
2020-04-29 00:24:11 +0200

    init commit

---
+++ App.js
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+
+      <Text>asd</Text>
+      <Text>asd</Text>
+    </SafeAreaView>
+
+
+  );
+};
+
+const styles = StyleSheet.create({
+})
+
+export default App;
