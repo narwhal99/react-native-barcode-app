18e3c52a186b72826c3cf4842f40c2ccfb300a0c
Molnár Andrej <molnarandre@outlook.com>
2020-04-30 21:18:55 +0200

    able to check if user is logged

--- App.js
+++ App.js
@@ -7,19 +7,82 @@ import scannedFood from './src/pages/scannedFood'
 import HomeScreen from './src/pages/home'
 import CameraScreen from './src/pages/camera'
 import LoginScreen from './src/pages/login'
+import UserScreen from './src/pages/user'
+import RegisterScreen from './src/pages/register'
+import AsyncStorage from '@react-native-community/async-storage'
+import axios from 'axios';
 
 const BottomTab = createBottomTabNavigator();
 const Stack = createStackNavigator();
+
 import {
   NavigationContainer,
   DefaultTheme,
   DarkTheme,
 } from '@react-navigation/native';
 
+
+
+
+const createCameraStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="CameraScreen" component={CameraScreen} />
+    <Stack.Screen name="scannedFood" component={scannedFood} />
+  </Stack.Navigator>
+);
+
+
+const createBottomTabs = () => (
+  <BottomTab.Navigator>
+    <BottomTab.Screen name="Home" component={HomeScreen}
+      options={{
+        tabBarIcon: () => (
+          <Icon style={[{ color: 'white' }]} size={25} name={'human'} />
+        )
+      }} />
+    <BottomTab.Screen name="Camera" children={createCameraStack} />
+    <BottomTab.Screen name="Felhasználó" component={UserScreen} />
+  </BottomTab.Navigator >
+)
+
+const AuthStackScreen = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Bejelentkezés" component={LoginScreen} />
+    <Stack.Screen name="Regisztráció" component={RegisterScreen} />
+  </Stack.Navigator>
+)
+
+
 export default App = () => {
+  const [user, setUser] = React.useState(null)
 
+  async function checkToken() {
+    let token = ''
+    try {
+      token = await AsyncStorage.getItem('token')
+    } catch (e) {
+      console.log(e)
+    }
+
+    if (!token) {
+      setUser(null)
+    } else {
+      await axios.get('http://192.168.1.72:8080/getUser', {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      }).then((data) => {
+        setUser(data.data.user)
+      }).catch(e => {
+        console.log(e.response.data)
+      })
+    }
+  }
+
+  React.useEffect(() => {
+    checkToken()
+  }, [])
   const colorScheme = useColorScheme();
-
   const MyTheme = {
     dark: false,
     colors: {
@@ -30,33 +93,13 @@ export default App = () => {
       border: 'green',
     },
   }
-
-  createCameraStack = () => {
-    return <Stack.Navigator>
-      <Stack.Screen name="CameraScreen" component={CameraScreen} />
-      <Stack.Screen name="scannedFood" component={scannedFood} />
-    </Stack.Navigator>
-  }
-
-  createBottomTabs = () => {
-    return (
-      <BottomTab.Navigator>
-        <BottomTab.Screen name="Home" component={HomeScreen}
-          options={{
-            tabBarIcon: () => (
-              <Icon style={[{ color: 'white' }]} size={25} name={'human'} />
-            )
-          }} />
-        <BottomTab.Screen name="Camera" children={this.createCameraStack} />
-        <BottomTab.Screen name="Login" component={LoginScreen} />
-      </BottomTab.Navigator >
-    )
-  }
-
   return (
     <AppearanceProvider>
       <NavigationContainer theme={colorScheme == 'dark' ? DarkTheme : MyTheme}>
-        {this.createBottomTabs()}
+        {user ? (
+          createBottomTabs()
+        ) : (
+            AuthStackScreen())}
       </NavigationContainer>
     </AppearanceProvider >
   )
---
+++ src/pages/register.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, SafeAreaView, Image } from 'react-native';
+
+export default RegisterTab = () => {
+    return (
+        <View>
+            <Text>Valami</Text>
+        </View>
+    )
+}
