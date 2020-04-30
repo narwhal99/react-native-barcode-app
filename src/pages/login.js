17b9e2e3093dd1eed61fccf8a71c9435a47ab88e
Molnár Andrej <molnarandre@outlook.com>
2020-04-30 18:02:41 +0200

    alpha login save and get token

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage'
 import { TextInput } from 'react-native-gesture-handler';
+import axios from 'axios';
 
-LoginTab = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.loginText}>Bejelentkezés</Text>
-            <TextInput style={styles.inputBox} placeholder="Email" />
-            <TextInput style={styles.inputBox} placeholder="Jelszó" secureTextEntry={true}/>
-            <Button title="Bejelentkezés"/>
-        </View>
-    )
+export default class LoginTab extends Component {
+    state = {
+        email: '',
+        password: ''
+
+    }
+    onChangeText = (key, value) => {
+        this.setState({ [key]: value })
+    }
+    signUp = () => {
+        const { email, password } = this.state
+        axios.post('http://192.168.1.72:8080/login', { email, password }).then((response) => {
+            AsyncStorage.setItem('token', response.data.token)
+            console.log(response.data.token)
+        }).catch((e) => {
+            console.log(e.response.data)
+        })
+    }
+    print = async () => {
+        console.log(await AsyncStorage.getItem('token'))
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.loginText}>Bejelentkezés</Text>
+                <TextInput style={styles.inputBox} placeholder="Email"
+                    onChangeText={val => this.onChangeText('email', val)} />
+                <TextInput style={styles.inputBox} placeholder="Jelszó"
+                    onChangeText={val => this.onChangeText('password', val)}
+                    secureTextEntry={true} />
+                <Button title="Bejelentkezés" onPress={this.signUp} />
+                <Button title="Token print" onPress={this.print} />
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
@@ -32,4 +60,3 @@ const styles = StyleSheet.create({
     }
 })
 
-export default LoginTab;
